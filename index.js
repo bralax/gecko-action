95127a92cdba56332aca765f3eae446255f1cb00
Brandon Lax <brlax@optonline.net>
2020-05-12 00:41:06 -0400

    create a metadata file

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const myToken = core.getInput('githubToken');
 
 //joining path of directory
 const directoryPath = process.env.GITHUB_WORKSPACE;
+const metadata = {questions: []};
 
-function printFiles(err, files) {
+function getQuestions(err, files) {
     //handling error
     if (err) {
         return console.log('Unable to scan directory: ' + err);
@@ -20,12 +21,55 @@ function printFiles(err, files) {
     files.forEach(function (file) {
         // Do whatever you want to do with the file
         ioUtil.isDirectory(file).then(r => {
-            if (r && !file.includes('.')) {
-                fs.readdir(path.join(__dirname, file), printFiles);
+            const num = /^\d+$/.test(file);
+            if (r && num) {
+                metadata.questions.push({questionNum: file, versions: []});
             }
         });
-        console.log(file);
     });
 }
 
-fs.readdir(directoryPath, printFiles);
+
+fs.readdir(directoryPath, getQuestions);
+for (let i = 0; i < metadata.questions.length; i++) {
+    fs.readdir(path.join(directoryPath, metadata.questions[i].questionNum),  (err, files) => {
+        //handling error
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }
+        //listing all files using forEach
+        files.forEach(function (file) {
+            // Do whatever you want to do with the file
+            ioUtil.isDirectory(file).then(r => {
+                const num = /^\d+$/.test(file);
+                if (r && num) {
+                    const question = metadata.questions.find((element) => {element.questionNum === i});
+                    question.versions.push({version: file, starterCodeFiles: []});
+                }
+            });
+        });
+    });
+    for (let j = 0; j < metadata.questions[i].versions.length; j++) {
+        fs.readdir(path.join(directoryPath, metadata.questions[i].questionNum, metadata.questions[i].versions[j].version),
+            (err, files) => {
+            //handling error
+            if (err) {
+                return console.log('Unable to scan directory: ' + err);
+            }
+            //listing all files using forEach
+            files.forEach(function (file) {
+                // Do whatever you want to do with the file
+                ioUtil.isDirectory(file).then(r => {
+                    if (!r && file != "Question.html") {
+                        const question = metadata.questions.find((element) => {element.questionNum === i});
+                        const version = question.versions.find((element) => {element.version === j});
+                        version.starterCodeFiles.push(file);
+                    }
+                });
+            });
+        });
+    }
+}
+
+
+console.log(metadata);
