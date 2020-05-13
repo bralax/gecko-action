c5d5dc8ae838979398c2bece657498b7d8b6c1d9
Brandon Lax <brlax@optonline.net>
2020-05-13 18:08:24 -0400

    Fail on Crash

--- index.js
+++ index.js
@@ -10,18 +10,13 @@ const geckoToken = core.getInput('geckoToken');
 const myToken = core.getInput('githubToken');
 const examid = core.getInput('examId');
 
-//joining path of directory
 const directoryPath = process.env.GITHUB_WORKSPACE;
 const metadata = {questions: [], examid: examid};
 
 function getQuestions(files) {
-    //listing all files using forEach
     let count = 0;
     files.forEach(function (file) {
-        console.log(file);
-        // Do whatever you want to do with the file
         let r = fs.lstatSync(path.join(directoryPath,file)).isDirectory();
-        console.log(!isNaN(file));
         if (r && !isNaN(file)) {
             metadata.questions.push({questionNum: parseInt(file), versions: []});
             count++;
@@ -45,22 +40,25 @@ function post(url, body, headers) {
     })
 }
 
+function failed(message) {
+    core.setFailed(message);
+    process.exit(1);
+}
+
 
 let files = fs.readdirSync(directoryPath);
 let count = getQuestions(files);
 if (count === 0) {
-    core.error("This repository does not contain any questions");
+    failed("This repository does not contain any questions");
 }
 for (let i = 0; i < metadata.questions.length; i++) {
     files = fs.readdirSync(path.join(directoryPath, "" + metadata.questions[i].questionNum));
-    //listing all files using forEach
     const qNum = metadata.questions[i].questionNum;
     if (files.length === 0) {
-        core.error("Quetion # " + qNum + " contains no versions");
+        failed("Quetion # " + qNum + " contains no versions");
     }
     files.forEach(function (file) {
         console.log(qNum + "/" + file);
-        // Do whatever you want to do with the file
         let r = fs.lstatSync(path.join(directoryPath,"" + qNum,"" + file)).isDirectory();
         if (r && !isNaN("" + file)) {
             const question = metadata.questions[i];
@@ -71,14 +69,10 @@ for (let i = 0; i < metadata.questions.length; i++) {
         files = fs.readdirSync(path.join(directoryPath, "" + metadata.questions[i].questionNum, "" + metadata.questions[i].versions[j].version));
         const qNum = metadata.questions[i].questionNum;
         const vNum = metadata.questions[i].versions[j].version;
-        //listing all files using forEach
         if (files.length === 0) {
-            core.error("Quetion # " + qNum + " Version # " + vNum + " contains no files");
+            failed("Quetion # " + qNum + " Version # " + vNum + " contains no files");
         }
         files.forEach(function (file) {
-
-            console.log(qNum + "/" + vNum + "/" + file);
-            // Do whatever you want to do with the file
             let r = fs.lstatSync(path.join(directoryPath,"" + qNum,"" + vNum,"" + file)).isDirectory();
             if (!r && file !== "Question.html") {
                 const version = metadata.questions[i].versions[j];
@@ -89,7 +83,6 @@ for (let i = 0; i < metadata.questions.length; i++) {
 }
 
 
-console.log(metadata);
 post("updateExamGithub", JSON.stringify(metadata)).then(data => {
     return data.text();
 }).then(data => {
@@ -104,7 +97,6 @@ post("updateExamGithub", JSON.stringify(metadata)).then(data => {
                     files.push(content);
                 });
                 const cur = path.join(directoryPath, "" + value.questionNum, "" + value1.version, "Question.html");
-                console.log(cur);
                 let instructionContent = fs.readFileSync(cur, {encoding: 'utf8'});
                 const formData = new FormData();
                 formData.append("examId" , examid);
@@ -112,19 +104,22 @@ post("updateExamGithub", JSON.stringify(metadata)).then(data => {
                 formData.append("version" , value1.version);
                 formData.append("files", JSON.stringify(files));
                 formData.append("fileNames", JSON.stringify(fileNames));
-                post("updateSC", formData);
+                post("updateSC", formData).catch(reason => {
+                    failed(reason);
+                });
                 const forms = new FormData();
                 forms.append("examId" , examid);
                 forms.append("questionNum" , value.questionNum);
                 forms.append("version" , value1.version);
                 forms.append("instructions", "" + instructionContent);
-                console.log(forms);
-                post("updateQP", forms);
+                post("updateQP", forms).catch(reason => {
+                    failed(reason);
+                });
 
             });
 
         }));
     }
 }).catch(reason => {
-    core.error(reason);
+    failed(reason);
 });
